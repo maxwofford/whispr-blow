fd0074d16aeb089bd8422aa7c24b40485ec84b76
Max Wofford <max@maxwofford.com>
2024-12-16 13:11:25 -0500

    Initial Commit

---
+++ whispr-blow/whispr_blowApp.swift
@@ -0,0 +1,17 @@
+//
+//  whispr_blowApp.swift
+//  whispr-blow
+//
+//  Created by Max Wofford on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct whispr_blowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ whispr-blowTests/whispr_blowTests.swift
@@ -0,0 +1,16 @@
+//
+//  whispr_blowTests.swift
+//  whispr-blowTests
+//
+//  Created by Max Wofford on 12/16/24.
+//
+
+import Testing
+
+struct whispr_blowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
