a7c8a520a7e460272ff71eb3d5983c3183175d7c
Max Wofford <max@maxwofford.com>
2024-12-16 14:41:03 -0500

    Setup basic transcription app

---
+++ whispr-blow/AudioTranscriptionView.swift
@@ -0,0 +1,62 @@
+//
+//  AudioTranscriptionView.swift
+//  whispr-blow
+//
+//  Created by Max Wofford on 12/16/24.
+//
+
+import Foundation
+import AudioKit
+import SwiftWhisper
+
+func convertAudioFileToPCMArray(fileURL: URL, completionHandler: @escaping (Result<[Float], Error>) ->Void) {
+    var options = FormatConverter.Options()
+    options.format = .wav
+    options.sampleRate = 16000
+    options.bitDepth = 16
+    options.channels = 1
+    options.isInterleaved = false
+    
+    let tempURL = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(UUID().uuidString)
+    let converter = FormatConverter(inputURL: fileURL, outputURL: tempURL, options: options)
+    
+    converter.start { error in
+        if let error {
+            completionHandler(.failure(error))
+            return
+        }
+        
+        let data = try! Data(contentsOf: tempURL)
+        
+        let floats = stride(from: 44, to: data.count, by: 2).map {
+            return data[$0..<$0 + 2].withUnsafeBytes { bytes in
+                let short = Int16(littleEndian: bytes.load(as: Int16.self))
+                return max(-1.0, min(Float(short) / 32767.0, 1.0))
+                
+            }
+        }
+        
+        try? FileManager.default.removeItem(at: tempURL)
+        
+        completionHandler(.success(floats))
+    }
+}
+
+func extractTextFromAudio(_ audioURL: URL, completionHandler: @escaping (Result<String, Error>) ->Void) {
+    let modelURL = Bundle.main.url(forResource: "tiny", withExtension: "bin")!
+    let whisper = Whisper(fromFileURL: modelURL)
+    
+    convertAudioFileToPCMArray(fileURL: audioURL) { result in
+        switch result {
+        case .success(let success):
+            Task {
+                do {
+                    let segments = try await whisper.transcribe(audioFrames: success)
+                    completionHandler(.success(segments.map(\.text).joined()))
+                }
+            }
+        case .failure(let failure):
+            completionHandler(.failure(failure))
+        }
+    }
+}
--- whispr-blow/ContentView.swift
+++ whispr-blow/ContentView.swift
@@ -7,13 +7,24 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State var listening: Bool = false
+    @State var audioTranscription: String?
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            Button(listening ? "Stop listening" : "Listen") {
+                self.listening.toggle()
+                if (listening) {
+                    
+                }
+            }
         }
         .padding()
     }
@@ -22,3 +33,4 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
